44f97bf38efd5c02e5314c8436a1aa772dec81a4
gildairmoreira <104792309+gildairmoreira@users.noreply.github.com>
2025-08-02 14:37:33 -0300

    feat: adicionando favicon e google analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 import { ptBR } from "@clerk/localizations";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -15,6 +16,9 @@ export const metadata: Metadata = {
   },
   description:
     "JobFlashAI é a maneira mais fácil de criar um currículo profissional que ajudará você a conquistar o emprego dos seus sonhos.",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
@@ -26,7 +30,17 @@ export default function RootLayout({
           // Justification: Temporary 'as any' due to type mismatch in Clerk's localization types. To be resolved in future updates.
       /* eslint-disable @typescript-eslint/no-explicit-any */
       <ClerkProvider localization={ptBR as any}>
-      <html lang="en" suppressHydrationWarning>
+      <html lang="pt-br" suppressHydrationWarning>
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-X4RVT0YTBC" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-X4RVT0YTBC');
+          `}
+        </Script>
         <body className={inter.className}>
           <ThemeProvider
             attribute="class"
